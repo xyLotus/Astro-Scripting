6aac5e2259baacc03befd84f0fe206d3ef4aa4f8
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-07 12:29:11 +0100

    0.2 - format: 2
    Main changes:
    1. added: `access-element` variable type
    2. changed the line comment from // to --
    3. added `import` statement
    4. added `call` statement
    5. changed `data` & `params` fields to `args`
    6. extended variable typing

--- asp.py
+++ asp.py
@@ -1,13 +1,28 @@
+# python >= 3.9
 """ This is the code parser for Astro Script Executable
 (.asx) files. It is supposed to be used as a plug-in script,
 not being executed as a standalone program. When using it as
-a module, just use the parse function. """
+a module, just use the parse function.
 
+  * 0.2
+    - changed the format to 2
+    - added parse_data(), which parses the data on the right of
+      an assignment type
+    - changed the line comment from `//` to `--`
+
+
+"""
 import llx
 import re
 
 __author__ = 'bellrise'
-__version__ = 0.1
+__version__ = 0.2
+
+# This is the format version of the code object generated
+# by the parser, each new format is most probably incompatible
+# with the older one, as names get changed and data shifted
+# around or added into other containers.
+FORMAT = 2
 
 
 class _Parser:
@@ -15,6 +30,7 @@ class _Parser:
 
     def __init__(self, fp):
         # Entry point for the parser.
+        self.lines = 0
         self.code = self.clean(fp.readlines())
 
     def err(self, line, linenum, *info):
@@ -32,7 +48,7 @@ class _Parser:
         in_comment = False
         for index, item in enumerate(lines):
             item: str
-            if item.strip().startswith('//'):
+            if item.strip().startswith('--'):
                 new.append(index)
             if in_comment:
                 new.append(index)
@@ -67,26 +83,72 @@ class _Parser:
                 return True
         return False
 
+    def classify_type(self, string: str):
+        # Returns a variable depending on the type defined in the
+        # string, raises an parsing error if the formatting is
+        # incorrect.
+        # Test for string
+        if string.count('"') != 2 and string.find('"') != -1:
+            self.err(string, 0, 'Quotes placement is incorrect')
+        if string.startswith('"') and string.endswith('"'):
+            return 'str', string.strip('"')
+        # Test for number
+        try:
+            return 'num', float(string)
+        except ValueError:
+            pass
+        # Test for variable
+        if re.match(r'[a-zA-Z_\d]', string):
+            return 'var', string
+        else:
+            self.err(string, 0, 'Unknown data format')
+
+    def parse_data(self, dat: str):
+        # Parses the data struct, returns a different data
+        # structure depending on the complexity of the
+        # data given.
+        dat = [self.classify_type(s.strip()) for s in dat.split(',')]
+        return dat
+
     def parse_statement(self, st: str):
         # Statement parser, returns a code object
-        st = st.strip().split(' ', 1)
-        try:
-            params = st[1]
-            if params.startswith('='):
-                params = params.replace('=', '').strip()
-                return {
-                    'type': 'assignment',
-                    'variable': st[0],
-                    'data': params
-                }
+        if len(st.split()) > 1:
+            ins, params = st.split(maxsplit=1)
+        else:
+            ins, params = "", ""
 
-        except IndexError:
-            params = ""
-        return {
-            'type': 'statement',
-            'instruction': st[0],
-            'params': params
-        }
+        # Function call
+        if re.match(r'^.*\(.*\).*', st):
+            func = self.parse_header(''.join([ins, params]), '')
+            return {
+                'type': 'call',
+                'name': func[0],
+                'args': self.parse_data(','.join(func[1]))
+            }
+
+        # Assignment
+        if re.match(r'^\w.*=.*', st):
+            if params[0] == '=':
+                p = params.strip('=').strip()
+            else:
+                self.err(st, 0, 'Cannot read assignment statement')
+                p = ""
+
+            return {
+                'type': 'assignment',
+                'variable': ins,
+                'args': self.parse_data(p)
+            }
+
+        # Any other statement
+        if not re.match(r'=', st):
+            return {
+                'type': 'statement',
+                'instruction': ins,
+                'args': self.parse_data(params)
+            }
+
+        self.err(st, 0, 'Invalid statement')
 
     def parse_collection(self, col: list):
         # Collection parser, returns a code object
@@ -99,16 +161,66 @@ class _Parser:
             'statements': statements
         }
 
-    def parse_header(self, head: str):
+    def parse_header(self, head: str, prefix='#'):
         # Function header parser, returns a tuple
-        header = head.replace(' ', '').replace('#', '').split('(')
-        if len(header) < 2 or not self.find_all(head, '(', ')', '#'):
+        header = head.replace(' ', '').replace(prefix, '').split('(')
+        if len(header) < 2 or not self.find_all(head, '(', ')', prefix):
             self.err(head, 0, 'Invalid function format')
         params = header[1].replace(')', '').replace(':', '').split(',')
-        #       name      *params
+
         return header[0], params
 
-    def statement_collector(self):
+    def imports(self, code):
+        # Finds all import statements and re-formats them
+        for index, i in enumerate(code):
+            if i['type'] == 'statement' and i['instruction'] == 'import':
+                imports = []
+                for p in i['args']:
+                    if p[0] != 'var':
+                        self.err('import', 0, 'Cannot import constant')
+                    imports.append(p[1])
+                i['modules'] = imports
+                i['type'] = 'import'
+                del i['args']
+                del i['instruction']
+                code[index] = i
+        return code
+
+    def args_access_element(self, st: dict):
+        # Finds all access-element statements
+        if 'args' in st:
+            for index, arg in enumerate(st['args']):
+                # Take only variables into consideration
+                if arg[0] != 'var':
+                    continue
+                var: str = arg[1]
+                if re.match(r'^.+\[\d+\]', var):
+                    if var.count('[') != 1 or var.count(']') != 1:
+                        self.err('Element access', 0, 'Incorrect amount of brackets')
+
+                    # Parsing element access
+                    name, num = var.split('[', maxsplit=1)
+                    num = num.strip(']')
+                    st['args'][index] = [
+                        'access-element',
+                        [name, num]
+                    ]
+
+        return st
+
+    def recollect(self, code):
+        # Recollects the parsed data, checks every type and replaces
+        # it with another.
+        # type->import
+        code = self.imports(code)
+
+        # params[n][0]->access-element
+        for index, st in enumerate(code):
+            code[index] = self.args_access_element(st)
+
+        return code
+
+    def collect(self):
         # Returns an object of statements and functions
         code = self.code
 
@@ -146,7 +258,8 @@ class _Parser:
 
     def render(self):
         # Returns the code object.
-        return self.statement_collector()
+        code = self.collect()
+        return self.recollect(code)
 
 
 def parse(fp):
