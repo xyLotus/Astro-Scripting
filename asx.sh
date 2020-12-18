a9b5fc8dc3df294d04788f8019579627fbd45f2e
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-18 10:07:09 +0100

    Linux entrypoint

---
+++ asx.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# This is a simple way of executing the python script on Linux
+# systems without needing to use 'python3 interpreter.py' before
+# the filename itself. This is just a quality of life thing,
+# no functionality will actually be found here. 
+python3 interpreter.py $1
