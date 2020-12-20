c3170633269653b69663235d407f73bc65f1367e
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-20 17:31:14 +0100

    Create StarBuilder.c

---
+++ starbuilder/StarBuilder.c
@@ -0,0 +1,17 @@
+/**
+ * This is the entrypoint for the Star Collector software. 
+ * 
+ * @author   bellrise
+ * @version  0.1
+ */
+#include <stdio.h>
+#include "b/Base.h"
+
+void testing();
+
+// Main entrypoint
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
