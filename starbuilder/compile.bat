0bdd1a10364eaf506952c226f3433467ba6528ca
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-20 19:24:32 +0100

    added -h

--- starbuilder/compile.bat
+++ starbuilder/compile.bat
@@ -3,21 +3,38 @@
 
 set run=false
 set compile=true
+set help=false
 
 set /A argc=0
 (for %%a in (%*) do (
     set /A argc = %argc% + 1
 
     :: Arguments
-    if %run%==false if %%a==-r set run=true
-    if %compile%==true if %%a==-n set run=false
+    if %run%==false if "%%a"=="-r" set run=true
+    if %run%==false if "%%a"=="--run" set run=true
+
+    if %compile%==true if "%%a"=="-n" set run=false
+    if %compile%==true if "%%a"=="--no-compile" set run=false
+
+    if %help%==false if "%%a"=="-h" set help=true
+    if %help%==false if "%%a"=="--help" set help=true
 
 ))
 
+:: Show help page
+if %help%==true call :help_page
+
 :: Compiling with gcc
-if %compile%==true gcc .\StarBuilder.c -o Starbuilder
+if %compile%==true gcc .\Star.c -o Starbuilder
 
 :: Run after compiling
-if %run%==true .\a
+if %run%==true .\Starbuilder
 
+EXIT 0
+
+:help_page
+    echo Usage: compile.bat [-r] [-n] [-h]
+    echo -h, --help        - Show this
+    echo -n, --no-compile  - Do not compile
+    echo -r, --run         - Run after compilation
 EXIT 0
