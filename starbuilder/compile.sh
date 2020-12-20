ba4c5020bdc8d61714cf48fbc9e9ffdf81753dcd
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-20 12:00:02 -0800

    Added configuration options for the linux script

--- starbuilder/compile.sh
+++ starbuilder/compile.sh
@@ -11,6 +11,7 @@ argv=("$@")
 opt_compile="true"
 opt_run="false"
 opt_help="false"
+opt_cfg="false"
 
 
 for arg in "${argv[@]}"; do
@@ -46,17 +47,54 @@ function print_help {
     exit 0
 }
 
+function load_config {
+    # Loads the config from the file
+    
+    cfg_main="StarBuilder.c"
+    cfg_name="StarBuilder"
+    cfg_bin="false"
+
+    local ln=0
+    while read line; do
+        
+        if [ ${line:0:1} != "#" ]; then 
+            # Non comment line, parse and set        
+            # Splitting the line
+            IFS='='
+            read -ra ADDR <<< "$line"
+            local setting=${ADDR[0]}
+            local data=${ADDR[1]}
+            
+            if [ $setting == "MAIN" ]; then cfg_main=$data; fi   
+            if [ $setting == "NAME" ]; then cfg_name=$data; fi   
+            if [ $setting == "BIN" ]; then cfg_bin=$data; fi   
+        fi
+
+        # Counter step
+        ln=$(( ln + 1 ))
+    done < starbuilder.config
+
+}
+
+# Load config before continuing to compilation
+load_config
 
 if [ $opt_help == "true" ]; then
     print_help
 fi  
 
 if [ $opt_compile == "true" ]; then
-    gcc ./StarBuilder.c -o StarBuilder  
+    gcc ./$cfg_main -o $cfg_name  
 fi  
 
 if [ $opt_run == "true" ]; then
-    chmod +x ./StarBuilder
-    ./StarBuilder
+    chmod +x ./$cfg_name
+    ./$cfg_name
 fi
+
+if [ $cfg_bin == "true" ]; then
+    sudo mv ./$cfg_name /bin/$cfg_name
+    rm ./$cfg_name
+fi
+
 
