6b93d7c37b2c8c9c28fe3b53867f185b2f061c61
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-21 07:43:03 -0800

    Finally, freedom - added multilevel code parsing

--- asp/asp3.py
+++ asp/asp3.py
@@ -1,19 +1,75 @@
 # python >= 3.6
-""" This is the code parser for Astro Script Executable
-(.asx) files. It is supposed to be used as a plug-in script,
-not being executed as a standalone program. When using it as
-a module, just use the parse function.
+""" This is the code parser for Astro Script Executable (.asx) files.
+It is supposed to be used as a plug-in script, not being executed as
+a standalone program. When using it as a module, just use the parse
+function. This has no dependencies apart from the Python standard
+library.
+
+* asp3: The third asx code format (FORMAT 3) will probably be supported
+  for quite some time, as it is the main parser ASX is based on, as of
+  December, 2020. The previous version called just asp, has been deprecated
+  as it was very buggy and had a lot problems and missing functionality.
+  asp3 will hopefully fix these problems by implementing a new way and
+  format for the asx code, so using it also becomes easier. Quick note:
+  the __version__ of the parser is defined with 3 numbers: the first one
+  represents the format, the second one shows the major version, and the
+  third the minor version. If two implementations have the same format
+  & major version, it is highly possible there should be no compatibilty
+  issues.
+
+* Versions: This version will be supported as long as the interpreter will
+  be using it, however parallel development of a new format can happen.
+  This style of work has been chosen because creating a new, stable parser
+  takes a lot of work and time, so in order to shorten the project development
+  downtime to a minimum, an older version will be supported & bugfixed
+  while a fresh and new version is being worked on. Every new version of
+  asp will be thoroughly tested before deployment, even if the beta is
+  alredy commited to the Github repository.
+
+* How it works: The main function of this whole parser would be the parse()
+  function, taking in the lines of the script as a list of strings, and
+  returning a JSON-serializable code object for the interpreter to read
+  and execute. Intenally, the parse() function creates an instance of the
+  _Parser class to call its get() method which returns the generated code
+  object (which happens already in the __init__ function. The constructor
+  takes in the list of strings, cleaning them up, removing comments and
+  calculating tabsizes. Note: a single tab can be any amount of spaces,
+  but then it has to be kept the same for every single indent, or the
+  parser will raise an IndentError which the interpreter can catch and
+  read.
+
+* Typing: After cleaning, each line of code is passed to the type() method,
+  which defines the type and class of the code, using regular expression
+  patterns. After that, each type is sent out to a different method returning
+  the parsed line. You can easily tell such a method as its name starts with
+  the 'parse_' prefix.
+
+* Sorting: After typing, the lines are sorted and put into blocks depending
+  on their indent size. All the different blocks can be found just scrolling
+  down a bit and looking at the 'BLOCKS' constant, containing a list of
+  block statement types.
+
+* Shifting: After sorting, the lines and blocks are shifted to the final
+  astro code compatible format from the parser-only internal format. The
+  final result is the JSON-serializable code object represented as a list
+  of statements (list of dicts).
+
 """
 import re
 
 __author__  = 'bellrise'
-__version__ = '3.1.4'
+__version__ = '3.3.4'
 
 # This is the format version of the code object generated
 # by the parser, each new format is most probably incompatible
 # with the older one, as names get changed and data shifted
 # around or added into other containers.
 FORMAT = 3
+
+# Blocks is a list of statement types which represent code
+# blocks, that are executed depending on the conditions.
+BLOCKS = ['if', 'else', 'elif', 'try', 'while', 'function']
+
 
 
 class _Parser:
@@ -32,7 +88,7 @@ class _Parser:
     def hash_strings(self, line, num):
         """ Replaces all strings with hashes for commenting
         purposes. """
-        matches = re.finditer('"[^"]+"', line)
+        matches = re.finditer('"[^"]*"', line)
         for match in matches:
             if match:
                 substr = line[match.start():match.end()]
@@ -57,7 +113,7 @@ class _Parser:
                 in_comment = True
                 commented.append(index)
 
-            if re.match('--/$', line.strip()):
+            if re.match('.*--/$', line.strip()):
                 # End of comment
                 if in_comment:
                     in_comment = False
@@ -90,12 +146,12 @@ class _Parser:
         for index, line in enumerate(self.code):
             match = re.match(r'^ *', line)
             if match.end() == 0:
-                self.code[index] = (index+1, 0, line.strip())
+                self.code[index] = [index+1, 0, line.strip()]
             # Size checking
             if match.end() % self.tabsize != 0:
                 raise IndentationError(f'Invalid tab size @ line {index}')
 
-            self.code[index] = (index+1, int(match.end() / self.tabsize), line.strip())
+            self.code[index] = [index+1, int(match.end() / self.tabsize), line.strip()]
 
     def type(self, lines):
         """ This is the main function for setting the types of the
@@ -136,105 +192,173 @@ class _Parser:
         for pos, line in enumerate(lines):
             text = line[2]
 
-            # Imports
-            if re.match(r'import .*', text):
-                lines[pos] = self.parse_import(line)
-                continue
+            structures = {
+                # Module import
+                r'import .*': self.parse_import,
 
-            # If statement
-            if re.match(r'^if .*:.*', text):
-                lines[pos] = self.parse_if(line)
-                continue
+                # If block header
+                r'^if .*:.*': self.parse_if,
 
-            # Else if statement
-            if re.match(r'^elif .*:.*', text):
-                lines[pos] = self.parse_elif(line)
-                continue
+                # While block header
+                r'^while .+:.*': self.parse_while,
 
-            # Else statement
-            if re.match(r'^else:', text):
-                lines[pos] = self.parse_else(line)
-                continue
+                # Elif block header
+                r'^elif .*:.*': self.parse_elif,
 
-            # Try statement
-            if re.match(r'^try:', text):
-                lines[pos] = self.parse_try(line)
-                continue
+                # Else block header
+                r'^else:': self.parse_else,
 
-            # Function header
-            if re.match(r'#[_A-z][_A-z0-9]*\(.*\):', text):
-                lines[pos] = self.parse_header(line)
-                continue
+                # Try block header
+                r'^try:': self.parse_try,
 
-            # Assignment
-            if re.match(r'^[_A-z][_A-z0-9]* *= *.*', text):
-                lines[pos] = self.parse_assignment(line)
-                continue
+                # Function block header
+                r'#[_A-z][_A-z0-9]*\(.*\):': self.parse_header,
 
-            # Base statement
-            if re.match(r'^[_A-z][_A-z0-9]* .*', text):
-                lines[pos] = self.parse_statement(line)
-                continue
+                # Assignment statement
+                r'^[_A-z][_A-z0-9]* *= *.*': self.parse_assignment,
 
-            # Function call
-            if re.match(r'^[_A-z][_A-z0-9]*\(.*\)', text):
-                lines[pos] = self.parse_call(line)
-                continue
+                # Regular base statement
+                r'^[_A-z][_A-z0-9]* .*': self.parse_statement,
 
-            if line[2]:
+                # Function call statement
+                r'^[_A-z][_A-z0-9]*\(.*\)': self.parse_call,
+
+            }
+
+            unparsed = True
+            for regex, method in structures.items():
+                if re.match(regex, text):
+                    lines[pos] = method(line)
+                    unparsed = False
+                    break
+
+            if unparsed and line[2]:
                 raise SyntaxError(f'Invalid syntax @ {line[0]}')
 
-        # Function recollection
-        for function in collected:
-            lines[function[0][0]-1] = self.sort_function(self.type(function))
 
-        # If statement recollection
-        found = True
-        while found:
-            found = False
-            collected = []
+        # I have to add a phantom line or the whole indent sorter
+        # will crash and burn and die and stop working, and this
+        # is a lot easier then having to write a pusher which would
+        # take another 30 lines. Trust me, I know what im doing.
+        lines.append([lines[-1][0]+1, 0, ''])
 
-            for index, line in enumerate(lines):
-                code = line[2]
-                if code:
-                    if code['type'] in ['if', 'else', 'elif', 'try']:
-                        found = True
-                        if_indent = line[1]
-                        cursor = index + 1
-                        while True:
-                            try:
-                                a = lines[cursor]
-                            except IndexError:
-                                break
-                            if a[1] == if_indent + 1:
-                                collected.append(a)
-                            else:
-                                break
-                            cursor += 1
-
-                        # Replacing
-
-                        d = lines[index][2]
-                        d.update({'code': collected})
-                        lines[index] = (lines[index][0], lines[index][1], d)
-
-                        for i in collected:
-                            p = i[0] - 1
-                            lines[p] = (lines[p][0], lines[p][1], '')
-
-                        collected = []
-
-                    if found:
-                        found = False
+        # Recursive sorting to reach every level of statements,
+        # also turns the code object format to such one that the
+        # interpreter can understand. This also removes all empty
+        # lines that don't need to be interpreted.
+        lines = self.recursive_sort(lines)
 
         return lines
 
-    def sort_function(self, lines):
-        """ Sorts the functions """
-        header = lines[0]
-        t = header[2]
-        t.update({'code': lines[1:]})
-        return header[0], header[1], t
+    def format(self, code):
+        """ Converts the parser format to a format that the interpreter
+        understands. """
+        b = {'line': code[0]}
+        b.update(code[2])
+        return b
+
+    def recursive_sort(self, lines):
+        """ Recursively calls sort_code_blocks for each 'code' field
+        found in any line. """
+        lines = self.sort_code_blocks(lines)
+
+        master = []
+
+        for index, line_ in enumerate(lines):
+            index = line_[0]
+            indent = line_[1]
+            code = line_[2]
+
+            # Empty code lines, not important
+            if not code:
+                continue
+
+            if 'code' in code:
+                code_ = self.recursive_sort(code['code'])
+                b: dict = code
+                b['code'] = code_
+                new_block = [index, indent, b]
+                master.append(self.format(new_block))
+            else:
+                master.append(self.format(line_))
+
+        return master
+
+    def sort_code_blocks(self, lines):
+        """ Sorts the code blocks into seperate blocks, depending
+        on their indentation level. """
+        new = lines.copy()
+
+        map_ = []
+        for i in new:
+            map_.append(i[0])
+
+        collected = []
+
+        cursor = 0
+        while cursor < len(lines) - 1:
+
+            indent = new[cursor][1]
+            code = new[cursor][2]
+
+            if not code:
+                cursor += 1
+                continue
+
+            block = []
+
+            # Basically checks if the type of the statement
+            # is a block type
+            if code['type'] in BLOCKS:
+                starting_indent = indent
+                block_header = new[cursor]
+                cursor += 1
+                while True:
+                    if cursor >= len(new):
+                        break
+
+                    if starting_indent != new[cursor][1]:
+                        block.append(new[cursor])
+                        collected.append(new[cursor][0])
+                    else:
+                        # Collect the statements into one pile
+                        pulled = new[map_.index(block_header[0])]
+                        d: dict = pulled[2]
+                        d.update({'code': block})
+                        new[map_.index(block_header[0])] = [pulled[0], pulled[1], d]
+                        block = []
+
+                        # Cleaning
+                        break
+
+                    cursor += 1
+
+                if block:
+                    pulled = new[map_.index(block_header[0])]
+                    d: dict = pulled[2]
+                    d.update({'code': block})
+                    new[map_.index(block_header[0])] = [pulled[0], pulled[1], d]
+
+                cursor -= 1
+
+            cursor += 1
+
+        cleared = []
+        for line in new:
+            if line[0] not in collected:
+                cleared.append(list(line))
+            else:
+                cleared.append([line[0], 0, ''])
+
+        another_cleared = []
+        for line in cleared:
+            if line[2]: another_cleared.append(line)
+
+        return another_cleared
+
+    # ------------------------------------------
+    # Tools
+    # ------------------------------------------
 
     def parse_math(self, line, num):
         """ Parses the mathematical thingy. """
@@ -316,6 +440,18 @@ class _Parser:
                 data = ('arr', elements)
                 return data
 
+            if re.match(r'\w+\.\w+', data):
+                call = data.split('.')[1]
+                if re.match(r'\w+\.\w+\(.*\)', data):
+                    # Function call
+                    call = self.parse_call((num, 0, data.split('.', maxsplit=1)[1]))
+                    call = call[2]
+                return ('call', {
+                    'module': data.split('.')[0],
+                    'name': call['name'],
+                    'params': call['params']
+                })
+
             if '"' not in data:
                 data = data.strip()
                 if re.match(r'.*\[[0-9]+\]', data):
@@ -359,10 +495,10 @@ class _Parser:
         text = line[2]
 
         text = text.replace(kw, '')[:-1].strip()
-        return (
+        return [
             index, indent,
             {'type': kw, 'condition': self.parse_math(text, index)}
-        )
+        ]
 
     def parse_elif(self, line):
         """ Parses an elif statement. """
@@ -370,11 +506,15 @@ class _Parser:
 
     def parse_else(self, line):
         """ Just returns a structured else. """
-        return line[0], line[1], {'type': 'else'}
+        return [line[0], line[1], {'type': 'else'}]
 
     def parse_try(self, line):
         """ Just returns a structures else. """
-        return line[0], line[1], {'type': 'try'}
+        return [line[0], line[1], {'type': 'try'}]
+
+    def parse_while(self, line):
+        """ Parses an while statemnt. """
+        return self.parse_if(line, kw='while')
 
     def parse_call(self, line: tuple):
         """ Parses a function call. """
@@ -385,10 +525,10 @@ class _Parser:
         name = text.split('(')[0]
         params = self.parse_args(text.split('(')[1][:-1], index)
 
-        return (
+        return [
             index, indent,
             {'type': 'call', 'name': name, 'params': params}
-        )
+        ]
 
     def parse_header(self, line: tuple):
         """ Parses a function header. """
@@ -407,10 +547,10 @@ class _Parser:
         # Parameters
         params = [s.strip() for s in params.split(',')]
 
-        return (
+        return [
             index, indent,
             {'type': 'function', 'name': name, 'parameters': params}
-        )
+        ]
 
     def parse_import(self, line: tuple):
         """ Parses an import statement. """
@@ -418,11 +558,11 @@ class _Parser:
             import_ = line[2].split()[1]
         except IndexError:
             raise SyntaxError(f'Import statement cannot be empty @ line {line[0]}')
-        return (
+        return [
             line[0],
             line[1],
             {'type': 'import', 'name': import_}
-        )
+        ]
 
     def parse_statement(self, line: tuple):
         """ Parses a regular base statement. """
@@ -437,10 +577,10 @@ class _Parser:
         else:
             ins, params = text, None
 
-        return (
+        return [
             index, indent,
             {'type': 'statement', 'name': ins, 'params': params}
-        )
+        ]
 
     def parse_assignment(self, line):
         """ Parses an assigment. """
@@ -452,44 +592,18 @@ class _Parser:
 
         data = self.variable(data, index)
 
-        return (
+        return [
             index, indent,
-            {'type': 'assignemnt', 'var': var, 'data': data}
-        )
+            {'type': 'assignment', 'var': var, 'data': data}
+        ]
 
     # ------------------------------------------
-    # Other
+    # Final
     # ------------------------------------------
-
-    def shift(self, statement):
-        """ Shifts the statement around to keep the asx format. """
-        d = statement[2]
-        d.update({'line': statement[0]})
-        return d
-
-    def collect(self, code):
-        """ Recollects the whole thing and adds a line number to each
-        statement. """
-        new = []
-        for line in code:
-            if line[0] not in self.omit and line[2]:
-                data = {'line': line[0]}
-                data.update(line[2])
-                new.append(data)
-
-        for index, st in enumerate(new):
-            if st['type'] in ['function', 'if', 'else', 'elif', 'try']:
-                sts = []
-                for i in st['code']:
-                    sts.append(self.shift(i))
-
-                new[index]['code'] = sts
-
-        return new
 
     def get(self):
         """ Returns the code object from the class. """
-        return self.collect(self.code)
+        return self.code
 
 
 def parse(lines: list):
