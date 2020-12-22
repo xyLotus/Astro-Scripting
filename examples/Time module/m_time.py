c7e4521bdea24bf5117dc6334cd4c82d68537233
bellrise <65973858+bellrise@users.noreply.github.com>
2020-12-22 18:33:47 +0100

    Example module

---
+++ examples/Time module/m_time.py
@@ -0,0 +1,16 @@
+import astropy as apy
+import datetime
+
+
+def f_time(scope: apy.Scope):
+    time_str = datetime.datetime.now().strftime('%H:%M:%S')
+    astro_string = apy.models.String.new('current_time', time_str)
+    scope.set_variable('current_time', astro_string)
+    
+    return scope.format()
+
+
+def __build__():
+    return [
+        apy.render(f_time, lib='Time', name='time')
+    ]
