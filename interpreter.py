3811537cfdb6af48dc6fd86f7cd5d25a4430edca
Lotus <59749700+xyLotus@users.noreply.github.com>
2020-12-29 18:06:52 +0100

    Changed Versions to x*10^-1

--- interpreter.py
+++ interpreter.py
@@ -1,75 +1,7 @@
 ''' ASX Interpreting / Execution '''
 
-''' Change Log:
-
-* 0.1
-    -- Memory Mangement Patch -- 
-    - AMM | Variable Storage Implemented
-    - AMM | Function Content Storage Implemented
-    - AMM | Function Parameter Name Storage Implemented
-
-* 0.2
-    -- Memory Management Patch -- 
-    - AMM | Parameter Name AND Value storage FINALLY FUCKING finished C: (thanks to bellrise)
-
-
-* 0.3 Pre-Patch 
-    -- Interpreter CLS Changes --
-    - 2 New Interpret Method Parameters
-    - 1st Parameter: in_function                        | For Interpreting Functions
-    - 2nd Parameter: function_name                      | For Interpreting Functions <- Getting Mem [AMM]
-
-* 0.3
-    -- Memory Handling Patch -- 
-    -- Statement Implementation -- 
-    - Say Statement Added                               | Output
-    - Wait Statement Added                              | Pausing Program
-    - Say Statement Parameter Mem Grabbing Implemented  | In/Out-Function (See 0.3 Pre-Patch)
-    - Wait Statement Parameter Mem Grabbing Implemented | In/Out-Function (See 0.3 Pre-Patch)
-    - Say Statement Variable Mem Grabbing Implemented   | In/Out-Function (See 0.3 Pre-Patch)
-    - Wait Statement Variable Mem Grabbing Implemented  | In/Out-Function (See 0.3 Pre-Patch)
-
-* 0.4
-    -- Interpreter CLS Changes --
-    -- Reformatting -- 
-    - Reformatted Interpreter CLS
-    - Code In "Interpret" Method Now Split Into Sub-Methods
-    - Interpret Method -> Main Method
-    - A Lot Of New Comments
-
-* 0.5
-    -- Memory Handling Patch --
-    - AMM | "function parameter value storage" deleted
-    - AMM | "function variable storage" merged with the "function parameter value storage"
-    - AMM | You can now change parameter values inside of the given function
-
-* 0.6
-    -- Argument Parsing -- 
-    - Added argparsing with the argparse module
-    - You are now able to execute files after setting up the ASX-Entrypoint
-    - ^ (See wiki page -> Setup Astro! [Not finished yet])
-
-* 0.7
-    -- Variable Error Handling -- 
-    - Added Variable Error Handling
-    - ^ Corresponding error: UndefinedVariable / UndefinedParameter
-    - The error_out function output changed for error specification
-    - Added force quitting the program if an error occurs
-
-* 0.8
-    -- Function Error Handling --
-    - Added Function Error Handling
-    - ^ Error Added: UndefinedFunction
-
-* 0.9
-    -- Array / List Output --
-    - Added multi value storing datatype output support
-    - [^ Thus meaning changes to the "_exec_say" function]
-    - Added correct String & Int output in list / array
-'''
-
 __author__ = 'Lotus'
-__version__ = '0.9'
+__version__ = '0.0.9'
 
 
 def error_out(error_message: str, ErrorType: str = 'ERROR'): 
